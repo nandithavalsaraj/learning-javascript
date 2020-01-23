84866385cd5095b90424597a36df190bf2f90c22
Nanditha Valsaraj <40928820+nvjvn@users.noreply.github.com>
2020-01-23 15:24:49 -0800

    pause and start button events are added. (#41)

--- projects/timer/index.js
+++ projects/timer/index.js
@@ -5,13 +5,24 @@ class Timer{
 		this.pauseButton = pauseButton;
 		//event listeners
 		this.startButton.addEventListener('click', this.start);
+		this.pauseButton.addEventListener('click', this.pause);
 	}
 	start = () => {
-		console.log(this);
+		this.tick();
+		//function tick is called every 1 second.
+		this.interval = setInterval(this.tick, 1000);
+		console.log(this.interval);
 	}
-}
+
+	pause = () => {
+		clearInterval(this.interval);
+	}
+
+	tick = () => {
+		console.log("tick");
+	}
+};
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const timer = new Timer(durationInput, startButton, pauseButton);
-timer.start();
