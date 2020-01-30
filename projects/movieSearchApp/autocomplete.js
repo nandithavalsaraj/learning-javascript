7127bd4d6b5b257832ab8f2a987e619dbff60dde
Nanditha Valsaraj <40928820+nvjvn@users.noreply.github.com>
2020-01-29 16:31:30 -0800

    -Update the template file (#45)
    - Include 2 searches

--- projects/movieSearchApp/autocomplete.js
+++ projects/movieSearchApp/autocomplete.js
@@ -1,13 +1,20 @@
-const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchData }) =>{
-	root.innerHTML =`
-		<label><b>Search For the Movie</b><label>
-		<input class ="input"/>
-		<div class="dropdown">
-    		<div class="dropdown-menu">
-    			<div class="dropdown-content results"></div>
-    		</div>
-    	</div>
-	`;
+
+const createAutoComplete = ({
+  root,
+  renderOption,
+  onOptionSelect,
+  inputValue,
+  fetchData
+}) => {
+  root.innerHTML = `
+    <label><b>Search</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+  `;
 
 	const input = root.querySelector('input');
 	const dropdown = root.querySelector('.dropdown');
@@ -19,7 +26,9 @@ const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fe
 			dropdown.classList.remove('is-active');
 			return;
 		}
-		resultsWrapper.innerHTML ='';
+
+		resultsWrapper.innerHTML = '';
+
 		dropdown.classList.add('is-active');
 		for(let item of items){
 			const option = document.createElement('a');
