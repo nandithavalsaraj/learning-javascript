23470b99fa2df027f0ee2dba7e817ca78de2e3e0
Nanditha Valsaraj <40928820+nvjvn@users.noreply.github.com>
2020-02-01 13:59:07 -0800

    Grid Generation for 3x3 Maze (#51)
    * Update the mixed shape game html
    
    * grid generation

--- projects/mazeGame/index.js
+++ projects/mazeGame/index.js
@@ -1,5 +1,5 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
-const width = 800;
+const { Engine, Render, Runner, World, Bodies} = Matter;
+const width = 600;
 const height = 600;
 const engine = Engine.create();
 const { world } = engine;
@@ -7,34 +7,30 @@ const render = Render.create({
 	element : document.body,
 	engine: engine,
 	options: {
-		wireframes : false,
+		wireframes : true,
 		width,
 		height
 	}
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
-World.add(world, MouseConstraint.create(engine, {
-	mouse : Mouse.create(render.canvas)
-}));
+
 //Walls
 const walls = [
-	Bodies.rectangle(400,0,800,40,{ isStatic : true }),
-	Bodies.rectangle(400,600,800,40,{ isStatic : true }),
-	Bodies.rectangle(0,300,40,600,{ isStatic : true }),
-	Bodies.rectangle(800,300,40,600,{ isStatic : true })
+	Bodies.rectangle(width / 2, 0, width, 40, { isStatic : true }),
+	Bodies.rectangle(width / 2, height, width, 40, { isStatic : true }),
+	Bodies.rectangle(0, height / 2, 40, height, { isStatic : true }),
+	Bodies.rectangle(width, height / 2, 40, height,{ isStatic : true })
 ];
 World.add(world, walls);
 
-//Adding random shapes
-for(let i =0; i < 50 ; i++){
-	if(Math.random() > 0.5){
-		World.add(world, Bodies.rectangle(Math.random()*width, Math.random()*height, 50, 50));
-	}
-	else{
-		World.add(
-			world,
-			Bodies.circle(Math.random()*width, Math.random()*height, 35)
-			);
-	}
-}
+//Maze generation
+const grid = Array(3)
+	.fill(null)
+	.map(() => Array(3).fill(false));
+const verticals =  Array(3)
+	.fill(null)
+	.map(() => Array(2).fill(false));
+const horizondal =  Array(2)
+	.fill(null)
+	.map(() => Array(3).fill(false));
