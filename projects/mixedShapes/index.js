7b2a76331ee0986d5013c67246b4c2df86481d86
Nanditha Valsaraj <40928820+nvjvn@users.noreply.github.com>
2020-01-31 20:32:38 -0800

    Including more shapes with colors (#49)

--- projects/mixedShapes/index.js
+++ projects/mixedShapes/index.js
@@ -1,17 +1,22 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
-
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const width = 800;
+const height = 600;
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
 	element : document.body,
 	engine: engine,
 	options: {
-		width : 800,
-		heignth : 600
+		wireframes : false,
+		width,
+		height
 	}
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
+World.add(world, MouseConstraint.create(engine, {
+	mouse : Mouse.create(render.canvas)
+}));
 //Walls
 const walls = [
 	Bodies.rectangle(400,0,800,40,{ isStatic : true }),
@@ -21,11 +26,19 @@ const walls = [
 ];
 World.add(world, walls);
 
-/*
-const shape = Bodies.rectangle(200, 200, 50, 50,{
-	isStatic : true
-});
+//Adding random shapes
+for(let i =0; i < 50 ; i++){
+	if(Math.random() > 0.5){
+		World.add(world, Bodies.rectangle(Math.random()*width, Math.random()*height, 50, 50));
+	}
+	else{
+		World.add(
+			world,
+			Bodies.circle(Math.random()*width, Math.random()*height, 35)
+			);
+	}
+}
+	
 
-World.add(world, shape);
-*/
+
 
