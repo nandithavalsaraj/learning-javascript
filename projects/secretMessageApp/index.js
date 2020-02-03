7c86086e516ffaf3ee59cbcbeca0bf82710efc06
nvjvn <nvj.valsaraj@gmail.com>
2020-02-02 17:51:29 -0800

    creating a linkInput variable

--- projects/secretMessageApp/index.js
+++ projects/secretMessageApp/index.js
@@ -2,6 +2,8 @@ document.querySelector('form').addEventListener('submit', event => {
 	event.preventDefault();
 	const input = document.querySelector('#message-input');
 	const encrypted = btoa(input.value);
-	document.querySelector('#link-input').value = encrypted;
+	const linkInput = document.querySelector('#link-input');
+	linkInput.value = `${window.location}#${encrypted}`;
+	linkInput.select();
 	
 });
