47e4021c1fb1d03fd809f0b9b0a6b6a407df2583
nvjvn <nvj.valsaraj@gmail.com>
2020-03-19 00:01:28 -0700

    Ecommerce app final commit

--- projects/ecommerce_app/index.js
+++ projects/ecommerce_app/index.js
@@ -1,41 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const usersRepo = require('./repositories/users');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
 
 const app = express();
+
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['lkasld235j']
+  })
+);
+app.use(authRouter);
+app.use(productsRouter);
+app.use(adminProductsRouter);
+app.use(cartsRouter);
 
-//route handler
-app.get('/', (req, res) => {
-	res.send(`
-		<div>
-			<form method="POST">
-				<input name="email" placeholder="email"/>
-				<input name="password" placeholder="password"/>
-				<input name="passwordconfirmation" placeholder="password confirmation"/>
-				<button>Sign Up</button>
-			</form>
-		</div>
-		`);
-});
-
-app.post('/', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('Email in use');
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
-  }
-  await usersRepo.create({email, password});
-  res.send('Account created!!!');
-});
-
-
-//tell app to start listening to the incomming network traffic
 app.listen(3000, () => {
-	console.log('listen');
+  console.log('Listening');
 });
